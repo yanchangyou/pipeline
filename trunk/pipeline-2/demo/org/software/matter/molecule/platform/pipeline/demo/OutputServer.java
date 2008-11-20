0bf3c65909d7d92732c0d02844221287d80fd65b
yanchangyou@gmail.com <yanchangyou@gmail.com@43c07c84-ada0-11dd-b268-c5ee67e421db>
2008-11-20 02:49:38 +0000

    完善:OutputServer 使其支持多线程, 每个请求用一个现场来处理

--- trunk/pipeline-2/demo/org/software/matter/molecule/platform/pipeline/demo/OutputServer.java
+++ trunk/pipeline-2/demo/org/software/matter/molecule/platform/pipeline/demo/OutputServer.java
@@ -1,48 +1,80 @@
 package org.software.matter.molecule.platform.pipeline.demo;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class OutputServer {
 	public static void main(String args[]) {
+		ServerSocket server = null;
 		try {
-			ServerSocket server = null;
+			server = new ServerSocket(10001);
+			// 创建一个ServerSocket在端口10001监听客户请求
+		} catch (Exception e) {
+			System.out.println("can not listen to:" + e);
+			// 出错，打印出错信息
+		}
+		Socket socket = null;
+
+		while (true) {
+
 			try {
-				server = new ServerSocket(10001);
-				// 创建一个ServerSocket在端口10001监听客户请求
-			} catch (Exception e) {
-				System.out.println("can not listen to:" + e);
-				// 出错，打印出错信息
-			}
-			Socket socket = null;
-			try {
+
 				socket = server.accept();
+				new Thread(new ServerThread(socket)).start();
 				// 使用accept()阻塞等待客户请求，有客户
 				// 请求到来则产生一个Socket对象，并继续执行
 			} catch (Exception e) {
 				System.out.println("Error." + e);
 				// 出错，打印出错信息
 			}
-			String line;
-			BufferedReader is = new BufferedReader(new InputStreamReader(socket
+		}
+	}
+}
+
+class ServerThread implements Runnable {
+	Socket socket;
+
+	ServerThread(Socket socket) {
+		this.socket = socket;
+	}
+
+	ServerThread() {
+
+	}
+
+	public void run() {
+
+		String line = null;
+		BufferedReader is = null;
+		try {
+			is = new BufferedReader(new InputStreamReader(socket
 					.getInputStream()));
+
 			// 由Socket对象得到输入流，并构造相应的BufferedReader对象
-//			PrintWriter os = new PrintWriter(socket.getOutputStream());
+			// PrintWriter os = new PrintWriter(socket.getOutputStream());
 			// 由Socket对象得到输出流，并构造PrintWriter对象
 			BufferedReader sin = new BufferedReader(new InputStreamReader(
 					System.in));
 			// 由系统标准输入设备构造BufferedReader对象
-			System.out.println(socket.getRemoteSocketAddress().toString().substring(1) + " speak : " + is.readLine()) ;
+			try {
+				System.out.println(socket.getRemoteSocketAddress().toString()
+						.substring(1)
+						+ " speak : " + is.readLine());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			// 在标准输出上打印从客户端读入的字符串
 			line = sin.readLine();
 			// 从标准输入读入一字符串r
 			while (!line.equals("bye")) {
 				// 如果该字符串为 "bye"，则停止循环
-//				os.println(line);
+				// os.println(line);
 				// 向客户端输出该字符串
-//				os.flush();
+				// os.flush();
 				// 刷新输出流，使Client马上收到该字符串
 				System.out.println("Server:" + line);
 				// 在系统标准输出上打印读入的字符串
@@ -51,13 +83,13 @@ public class OutputServer {
 				line = sin.readLine();
 				// 从系统标准输入读入一字符串
 			} // 继续循环
-//			os.close(); // 关闭Socket输出流
+			// os.close(); // 关闭Socket输出流
 			is.close(); // 关闭Socket输入流
 			socket.close(); // 关闭Socket
-			server.close(); // 关闭ServerSocket
-		} catch (Exception e) {
-			System.out.println("Error:" + e);
-			// 出错，打印出错信息
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
+
 }
