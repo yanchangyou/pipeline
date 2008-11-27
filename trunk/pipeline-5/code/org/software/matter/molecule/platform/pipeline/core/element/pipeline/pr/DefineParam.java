51e1dfdd057855ea36ed2a73b9a86a0dce3867b2
yanchangyou@gmail.com <yanchangyou@gmail.com@43c07c84-ada0-11dd-b268-c5ee67e421db>
2008-11-27 14:22:55 +0000

    定义版本 version = 5$0$0.0$0$0.1$0$0.20081127$2212 采用新的版本定义方式---扩展四段式版本定义法 观念父版本号$观念子版本号$观念测试版本号.结构父版本号$结构子版本号$结构测试版本号.功能父版本号$功能子版本号$功能测试版本号.日期$时间
    此版本定义法有了绝对的扩展性, 灵活性, 通用性, 时效性, 含义丰富性......
    测试版本号为0, 表示没有测试
    测试版本号代表了测试的次数
    
    功能新增:
    1, 数据类型定义---字符串(string), 数字(number), 日期(date), 盒子(box)
    
    功能修改:
    1, 变量访问方式---(this|super)(.box)*@(string|number|date)
       this代表了当前执行的unit的上下文
       super代表了当前执行的unit的父上下文(所有在它之上的上下文)

--- trunk/pipeline-5/code/org/software/matter/molecule/platform/pipeline/core/element/pipeline/pr/DefineParam.java
+++ trunk/pipeline-5/code/org/software/matter/molecule/platform/pipeline/core/element/pipeline/pr/DefineParam.java
@@ -1,4 +1,39 @@
 package org.software.matter.molecule.platform.pipeline.core.element.pipeline.pr;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DefineParam extends DRPR {
+
+	public final static String STRING_TYPE = "string";
+
+	public final static String NUMBER_TYPE = "number";
+
+	public final static String DATE_TYPE = "date";
+
+	public final static String BOX_TYPE = "box";
+	
+	public final static Set ALL_TYPE_SET = new HashSet(4);
+	
+	static {
+		ALL_TYPE_SET.add(STRING_TYPE);
+		ALL_TYPE_SET.add(NUMBER_TYPE);
+		ALL_TYPE_SET.add(DATE_TYPE);
+		ALL_TYPE_SET.add(BOX_TYPE);
+	}
+
+	protected String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public boolean containType(String type) {
+		return ALL_TYPE_SET.contains(type);
+	}
+
 }
