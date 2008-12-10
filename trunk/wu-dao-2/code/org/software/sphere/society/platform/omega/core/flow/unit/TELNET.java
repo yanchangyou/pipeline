af9bcad937581a2979d9d373c0c71cd0c1dc7faf
yanchangyou@gmail.com <yanchangyou@gmail.com@43c07c84-ada0-11dd-b268-c5ee67e421db>
2008-12-10 07:55:16 +0000

    维护 : 1, 简单实现telnet执行单元类, 只是已经简单的输出

--- trunk/wu-dao-2/code/org/software/sphere/society/platform/omega/core/flow/unit/TELNET.java
+++ trunk/wu-dao-2/code/org/software/sphere/society/platform/omega/core/flow/unit/TELNET.java
@@ -1,6 +1,8 @@
 package org.software.sphere.society.platform.omega.core.flow.unit;
 
+import java.io.PrintWriter;
 import java.net.ConnectException;
+import java.net.Socket;
 
 import org.software.sphere.society.platform.omega.core.execute.Session;
 
@@ -9,6 +11,13 @@ public class TELNET extends Unit {
 	public void execute(Session clientSession) throws ConnectException, Exception {
 		// TODO Auto-generated method stub
 		System.out.println("run telnet");
+		
+		Socket socket = clientSession.getClientSocket();
+		
+		PrintWriter os = new PrintWriter(socket.getOutputStream());
+		os.println("这是一个telnet服务, 目前还没有实现");
+		os.flush();
+//		socket.close();
 	}
 
 }
