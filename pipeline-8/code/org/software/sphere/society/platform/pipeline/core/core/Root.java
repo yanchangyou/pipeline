dff260bc693f1c7f8e0aba4a76911511647d56fd
yanchangyou@gmail.com <yanchangyou@gmail.com@43c07c84-ada0-11dd-b268-c5ee67e421db>
2008-12-14 18:13:45 +0000

    修改: 1, 修改流程多重嵌套不起作用的bug, 现在完美的支持的嵌套, 可以任意嵌套, 来源与从新学习digester类, 使用*/pattern的形式

--- pipeline-8/code/org/software/sphere/society/platform/pipeline/core/core/Root.java
+++ pipeline-8/code/org/software/sphere/society/platform/pipeline/core/core/Root.java
@@ -10,6 +10,7 @@ import org.software.sphere.society.platform.pipeline.core.data.DataNode;
 import org.software.sphere.society.platform.pipeline.core.data.node01.DefaultNode01;
 import org.software.sphere.society.platform.pipeline.core.flow.FlowNode;
 import org.software.sphere.society.platform.pipeline.core.real.Global;
+import org.software.sphere.society.platform.pipeline.core.real.RealNode;
 import org.software.sphere.society.platform.pipeline.exception.core.data.PreNodeNotFountException;
 import org.xml.sax.SAXException;
 
@@ -41,8 +42,62 @@ public class Root extends DefaultNode01 {
 		URL rulesUrl = Root.class.getClassLoader().getResource(VALIDATOR_RULES);
 
 		URL xmlUrl = Root.class.getClassLoader().getResource(PATH);
+		
+		
+		
+		
 		Digester digester = DigesterLoader.createDigester(rulesUrl);
 
+//		Digester digester = new Digester();
+//		digester.push(new Root());
+//		
+//		java.lang.String root = "root";
+//		java.lang.String global = "global";
+//		java.lang.String economy = "economy";
+////		java.lang.String root = "root";
+////		java.lang.String root = "root";
+////		java.lang.String root = "root";
+////		java.lang.String root = "root";
+//
+//		java.lang.String[] real_pattern_array = new java.lang.String[]{"*/global", "*/economy", "*/market", "*/supplier", "*/server"};
+//		java.lang.Class[] real_class_array = new java.lang.Class[]{Global.class, Economy.class, Market.class, Supplier.class, Server.class};
+//		
+//		for (int i = 0; i < real_pattern_array.length; i++) {
+//			digester.addObjectCreate(real_pattern_array[i], real_class_array[i]);
+//			digester.addSetProperties(real_pattern_array[i]);
+//			digester.addSetNext(real_pattern_array[i], "addNextRealNode");
+//		}
+//
+//		java.lang.String[] string_pattern_array = new java.lang.String[]{"*/string"};
+//		java.lang.Class[] string_class_array = new java.lang.Class[]{String.class};
+//		
+//		for (int i = 0; i < string_pattern_array.length; i++) {
+//			digester.addObjectCreate(string_pattern_array[i], string_class_array[i]);
+//			digester.addSetProperties(string_pattern_array[i]);
+//			digester.addCallMethod(string_pattern_array[i], "fromJavaString", 0);
+//			digester.addSetNext(string_pattern_array[i], "addDataNode", DataNode.class.getName());
+//		}
+//		
+//		java.lang.String[] service_pattern_array = new java.lang.String[]{"*/service"};
+//		java.lang.Class[] service_class_array = new java.lang.Class[]{ServiceNode.class};
+//		
+//		for (int i = 0; i < service_pattern_array.length; i++) {
+//			digester.addObjectCreate(service_pattern_array[i], service_class_array[i]);
+//			digester.addSetProperties(service_pattern_array[i]);
+//			digester.addSetNext(service_pattern_array[i], "addServiceNode", ServiceNode.class.getName());
+//		}
+//		
+//		java.lang.String[] competitor_pattern_array = new java.lang.String[]{"*/competitor"};
+//		java.lang.Class[] competitor_class_array = new java.lang.Class[]{Competitor.class};
+//		
+//		for (int i = 0; i < competitor_pattern_array.length; i++) {
+//			digester.addObjectCreate(competitor_pattern_array[i], competitor_class_array[i]);
+//			digester.addSetProperties(competitor_pattern_array[i]);
+//			digester.addSetNext(competitor_pattern_array[i], "addCompetitor", Competitor.class.getName());
+//		}
+//		
+		
+		
 		return (Root) digester.parse(new File(xmlUrl.getFile()));
 	}
 	
@@ -69,9 +124,9 @@ public class Root extends DefaultNode01 {
 		return (Global) this.getNext();
 	}
 
-	public void setGlobal(Global global) {
-		this.setNext(global);
-		global.setPreNode(this);
+	public void addNextRealNode(RealNode realNode) {
+		this.setNext(realNode);
+		realNode.setPreNode(this);
 	}
 	/**
 	 * 获取执行的入口, 和java语言类似, java从main开始, 而pipeline从第一个名为self的流程节点开始执行<br>
