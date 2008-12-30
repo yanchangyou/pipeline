ec237b11d0bf7f9f9f7ad24255a4fc22fec2a9ce
yanchangyou@gmail.com <yanchangyou@gmail.com@43c07c84-ada0-11dd-b268-c5ee67e421db>
2008-12-30 15:36:28 +0000

    测试 <pipeline-15> 	<代号>抽象</代号> 	<口号>发掘</口号> 	<实体交互方式> 		听说读写 	</实体交互方式> 	<哲学含义> 		<听说>意识实体与意识实体之间的交互</听说> 		<读写>意识实体与虚拟实体之间的交互</读写> 	</哲学含义> 	<改进> 		工作就是根据流水线来运用能力 		<工作> 			<直接工作> 			</直接工作> 			<根据资源工作> 				解决参数传递问题 			</根据资源工作> 			<根据流水线工作> 				解决流程安排问题 			</根据流水线工作> 		</工作> 	</改进> </pipeline-15>

---
+++ pipeline-15/测试/org/software/compont/atom/test/lang/中文类名.java
@@ -0,0 +1,8 @@
+package org.software.compont.atom.test.lang;
+
+public class 中文类名 {
+
+	public static void main(String[] args) {
+		System.out.println("能运行!");
+	}
+}
---
+++ pipeline-15/测试/org/software/compont/atom/test/mvel/MVELTest.java
@@ -0,0 +1,45 @@
+package org.software.compont.atom.test.mvel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mvel.MVEL;
+
+public class MVELTest {
+
+	public static void main(String[] args) {
+		simpleTest();
+//		varDefineTest();
+//		flowDefineTest();
+	}
+	
+	public static void simpleTest() {
+		Map map = new HashMap();
+		map.put("中文变量", "中文变量值");
+		Object result = MVEL.eval("中文变量 + \"end\" ", map);
+		System.out.println(result);
+	}
+	
+	public static void varDefineTest() {
+		Map map = new HashMap();
+		map.put("a", null);
+		String express = "a = 10";
+		
+		Object result = MVEL.eval(express, map);
+		
+		System.out.println(result);
+	}
+	
+	public static void flowDefineTest() {
+//		Map map = new HashMap();
+//		map.put("a", null);
+		String express = "for (int i =0; i < 100; i++) {  System.out.println(i); }";
+		
+		Object result = MVEL.eval(express);
+		
+		System.out.println(result);
+	}
+	
+	
+	
+}
