24799fb899236b1b469c242d3db1e26641c9cc41
yanchangyou@gmail.com <yanchangyou@gmail.com@43c07c84-ada0-11dd-b268-c5ee67e421db>
2009-01-08 13:47:51 +0000

    导入pipeline-18 <pipeline-18> 	<代号>神</代号> 	<口号>无</口号> </pipeline-17>

---
+++ pipeline-18/代码/日志.java
@@ -0,0 +1,8 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public interface 日志 {
+
+	public static Log 日志 = LogFactory.getLog(日志.class);
+}
---
+++ pipeline-18/测试/测试.java
@@ -0,0 +1,9 @@
+
+public class 测试 {
+
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		神 济公 = (神) Class.forName("地神").newInstance();
+		
+		System.out.println(济公);
+	}
+}
