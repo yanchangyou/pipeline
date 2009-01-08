c40e5eb93670fda77cad573a6b5538e210ea3ee8
yanchangyou@gmail.com <yanchangyou@gmail.com@43c07c84-ada0-11dd-b268-c5ee67e421db>
2009-01-08 13:47:51 +0000

    导入pipeline-18 <pipeline-18> 	<代号>神</代号> 	<口号>无</口号> </pipeline-17>

---
+++ trunk/pipeline-18/代码/神.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.digester.Digester;
+import org.apache.commons.digester.xmlrules.DigesterLoader;
+import org.xml.sax.SAXException;
+
+/**
+ * 全能的神<br>
+ * <br>
+ * 编写神要 三个东西<br>
+ * 1, 一个java类<br>
+ * 2, 一个digester解析文件<br>
+ * 3, 一个pipeline文件<br>
+ * 
+ * 神会根据这三者自动构建出神<br>
+ * <br>
+ * @author yanchangyou@gmail.com
+ * @date : 2009-1-8 下午09:18:49
+ * @file : 神.java
+ * @version : 0.1
+ */
+public abstract class 神 implements 日志 {
+	
+	/**
+	 * 初始化神
+	 * @throws IOException
+	 * @throws SAXException
+	 */
+	public 神() throws IOException, SAXException {
+		/**
+		 * digester 文件
+		 */
+		URL digesterFileURL = this.getClass().getResource(this.getClass().getName() + ".dg.xml");
+		
+		日志.info(digesterFileURL);
+		
+		/**
+		 * pipeline 文件
+		 */
+		URL pipelineFileURL = this.getClass().getResource(this.getClass().getName() + ".pl.xml");
+		
+		日志.info(pipelineFileURL);
+		
+		/**
+		 * 生成digester对象用于解析
+		 */
+		Digester digester = DigesterLoader.createDigester(digesterFileURL);
+		
+		/**
+		 * 把神自己传入, digester栈中第一个元素
+		 */
+		digester.push(this);
+		
+		/**
+		 * 开始解析pipeline文件构建神的灵魂
+		 */
+		digester.parse(new File(pipelineFileURL.getFile()));
+	}
+}
---
+++ trunk/pipeline-18/地神/地神.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+
+
+public class 地神 extends 神 {
+
+	private String 姓名;
+
+	public String get姓名() {
+		return 姓名;
+	}
+
+	public void set姓名(String 姓名) {
+		this.姓名 = 姓名;
+	}
+
+	public String toString() {
+		return this.get姓名();
+	}
+	
+	public 地神() throws IOException, SAXException {
+		super();
+	}
+}
