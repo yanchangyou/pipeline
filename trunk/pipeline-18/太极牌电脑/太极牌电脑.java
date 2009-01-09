5a506c1091f4d9d1c64ce221fa4bea565c330c82
yanchangyou@gmail.com <yanchangyou@gmail.com@43c07c84-ada0-11dd-b268-c5ee67e421db>
2009-01-09 08:07:38 +0000

    添加太极电脑

---
+++ trunk/pipeline-18/太极牌电脑/太极牌电脑.java
@@ -0,0 +1,15 @@
+
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+
+import 核心.抽象类.电脑;
+
+
+public class 太极牌电脑 extends 电脑 {
+
+	public 太极牌电脑() throws IOException, SAXException {
+		super();
+	}
+
+}
