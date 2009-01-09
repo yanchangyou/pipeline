af008b465f4c12889f51ef90e06a7174e972b197
yanchangyou@gmail.com <yanchangyou@gmail.com@43c07c84-ada0-11dd-b268-c5ee67e421db>
2009-01-09 07:40:45 +0000

    修改切换空间

--- trunk/pipeline-18/框架/共用/可统一的.java
+++ trunk/pipeline-18/框架/共用/可统一的.java
@@ -1,6 +1,6 @@
 package 共用;
 
-public interface 可统一的 extends 有名字的, 有状态的, 可被引用的, 有顺序的 {
+public interface 可统一的 extends 有名字的, 有状态的, 可被引用的, 有顺序的, 可日志输出的 {
 	
 	
 	
--- trunk/pipeline-18/测试/测试.java
+++ trunk/pipeline-18/测试/测试.java
@@ -1,11 +1,25 @@
+import 核心.抽象类.电脑;
 import 核心.节点.乾坤包节点;
 
 
 public class 测试 {
 
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		单点测试();
+		太极牌电脑测试();
+	}
+
+	public static void 单点测试() throws InstantiationException,
+			IllegalAccessException, ClassNotFoundException {
 		乾坤包节点 济公 = (乾坤包节点) Class.forName("地神").newInstance();
 		
 		System.out.println(济公);
+	}
+	
+	public static void 太极牌电脑测试() throws InstantiationException,
+		IllegalAccessException, ClassNotFoundException {
+		电脑 太极牌电脑 = (电脑) Class.forName("太极牌电脑").newInstance();
+	
+		System.out.println(太极牌电脑);
 	}
 }
