31782f426deee9d40dec9a1691498f99c6409fe2
yanchangyou@gmail.com <yanchangyou@gmail.com@43c07c84-ada0-11dd-b268-c5ee67e421db>
2009-01-09 08:03:41 +0000

    修改不够

---
+++ trunk/pipeline-18/地神/地神.java
@@ -0,0 +1,15 @@
+
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+
+import 核心.节点.乾坤包节点;
+
+
+public class 地神 extends 乾坤包节点 {
+
+	public 地神() throws IOException, SAXException {
+		super();
+	}
+
+}
